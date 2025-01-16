f1f1415ab7023d5c8b63fd9620b96f29a937f07a
Sushma25-ux <stetali@uab.edu>
2025-01-16 14:04:06 -0600

    new_changes

--- Lab1.c
+++ Lab1.c
@@ -5,9 +5,11 @@ int main(){
     printf("Enter the value: ");
     scanf("%d", &given_number);
     if(given_number % 2 == 0){
+        //Print the below text if the given number is even
         printf("the given number is an even value");
     }
     else{
+        //Print the below text if the given number is odd
         printf("the given number is a odd value");
     }
     return 0;
